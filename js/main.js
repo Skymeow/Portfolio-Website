7a36efae29005e1ed9df322a42a3530fe441dcd3
Sky Xu <skyrocks88xmz@gmail.com>
2017-04-10 17:42:07 -0400

    finishing

--- js/main.js
+++ js/main.js
@@ -1,11 +1,32 @@
 $(document).ready(()=>{
   console.log("JS is ready")
-  // $(function(){
-  //   $(".element").typed({
-  //     strings: ["Hi, my name is Sky","Sky is a <strong>web developer</strong>", "Sky is a <strong>desinger</strong>"],
-  //     typeSpeed: 0.2
-  //   });
-  // });
+    $("#typed").typed({
+        strings: ["a web developer.", "a web designer.", "Sky."],
+        typeSpeed: 30,
+        callback: function(){
+            lift();
+        }
+    });
+    function lift(){
+        $(".head-text").addClass("lift-text");
+    }
+  var x = $(".intro-one").offset();
+  var controller = new ScrollMagic.Controller();
+  var wipeAnimation = new TimelineMax()
+     .to("#slide-container", 1, {x: "-75%"})
+  new ScrollMagic.Scene({
+    triggerElement: "#pin-container",
+    triggerHook: "onLeave",
+    duration: "500%"
+  })
+    .setPin("#pin-container")
+    .setTween(wipeAnimation)
+    .addTo(controller);
 
+  var horizontal = new ScrollMagic.Scene({
+    offset: 50,
+    duration: 300,
+  })
+    .addTo(controller);
 
 });
