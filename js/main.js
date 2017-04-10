6741e4b420ba68384351f73bb4897c0fc8f2fcff
Sky Xu <skyrocks88xmz@gmail.com>
2017-04-10 11:17:24 -0400

    sync

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 $(document).ready(()=>{
   console.log("JS is ready")
+  // $(function(){
+  //   $(".element").typed({
+  //     strings: ["Hi, my name is Sky","Sky is a <strong>web developer</strong>", "Sky is a <strong>desinger</strong>"],
+  //     typeSpeed: 0.2
+  //   });
+  // });
+
+
 });
