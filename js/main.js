0489611f667a8f63fba0799ea2ebc274b1d1848a
Sky Xu <skyrocks88xmz@gmail.com>
2017-04-17 11:20:07 -0400

    add more slide

--- js/main.js
+++ js/main.js
@@ -3,30 +3,36 @@ $(document).ready(()=>{
     $("#typed").typed({
         strings: ["a web developer.", "a web designer.", "Sky."],
         typeSpeed: 30,
+        startDelay: 1000,
         callback: function(){
             lift();
         }
-    });
+    })
+
+
+   setTimeout
+
     function lift(){
         $(".head-text").addClass("lift-text");
     }
-  var x = $(".intro-one").offset();
+
+  var x = $(".intro-three").offset();
   var controller = new ScrollMagic.Controller();
   var wipeAnimation = new TimelineMax()
-     .to("#slide-container", 1, {x: "-75%"})
+     .to("#slide-container", 1, {x: "-80%"})
   new ScrollMagic.Scene({
     triggerElement: "#pin-container",
     triggerHook: "onLeave",
-    duration: "500%"
+    duration: "600%"
   })
     .setPin("#pin-container")
     .setTween(wipeAnimation)
     .addTo(controller);
 
-  var horizontal = new ScrollMagic.Scene({
-    offset: 50,
-    duration: 300,
-  })
-    .addTo(controller);
+  // var horizontal = new ScrollMagic.Scene({
+  //   offset: 50,
+  //   duration: 300,
+  // })
+  //   .addTo(controller);
 
 });
